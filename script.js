12ec39a29e65d9d89bf763bdbf97780cce5ac31c
sabreen salama <sabreensalama29@gmail.com>
2020-02-11 22:14:30 +0200

    gui plus js validation & storage

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 class User {
-    constructor(name,DOB,email,password,status,id)
+    constructor(name,DOB,email,password,phone,status,id)
 {
 this.name=name;
 this.DOB=DOB;
 this.email=email;
 this.password=password;
+this.phone=phone
 this.status=status;
 this.id=id;
 }
@@ -27,4 +28,44 @@ return archive;
     
 return archive;
  }
+}
+
+
+
+
+// //var user=new User("sabreen","1996-05-16","sabreensalama29@gmail.com","1234","01122661983");
+// User.storeObjectllo(user)
+
+// var user1=new User("salma","1996-05-16","sabreensalama@gmail.com","1234","01122661983");
+// User.storeObjectllo(user1)
+
+// session storage
+myemail="sabreensalama@gmail.com"
+sessionStorage.setItem("onlineuser",JSON.stringify(myemail));
+
+var sessionobj=JSON.parse(User.getObjectbyEmail(myemail));
+window.addEventListener("load", setInputField);
+function setInputField()
+{
+username.value=sessionobj.name
+password.value=sessionobj.password
+phone.value=sessionobj.phone
+email.value=sessionobj.email
+dob.value=sessionobj.DOB
+}
+
+
+submitButton.addEventListener("click",editObject)
+function editObject()
+{
+ sessionobj.name=username.value;
+ sessionobj.password=password.value;
+ sessionobj.phone=phone.value;
+ sessionobj.email=email.value;
+ sessionobj.DOB=dob.value
+ User.storeObjectllo(sessionobj)
+ setInputField(sessionobj)
+ console.log("your data saved")
+
+
 }
