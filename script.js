5f8c2e448ba07c797da068d6c9ebda7c72f01167
Muhammad ALsaied <muhammadalsaied96@gmail.com>
2020-02-12 10:56:56 +0200

    sign up

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
 class User {
-    constructor(name,DOB,email,password,status,id)
-{
-this.name=name;
-this.DOB=DOB;
-this.email=email;
-this.password=password;
-this.status=status;
-this.id=id;
-}
-static storeObjectllo = function (userObject) {
-    localStorage.setItem(userObject.email, JSON.stringify(userObject));
-}
-static getAllObjects = function (){
-    var archive = {}, // Notice change here
-    keys = Object.keys(localStorage),
-    i = keys.length;
-
-    while ( i-- ) {
-    archive[ keys[i] ] = localStorage.getItem( keys[i] );
+    constructor(name, DOB, email, password, status, id) {
+        this.name = name;
+        this.DOB = DOB;
+        this.email = email;
+        this.password = password;
+        this.status = status;
     }
-return archive;
- }
- static getObjectbyEmail = function (email){
-    
-    var archive = localStorage.getItem(email);
-    
-return archive;
- }
+    static storeObjectllo = function (userObject) {
+        if (!id) {
+            const lastInsertedId = JSON.parse(JlocalStorage.getItem("lastInsertedId"));
+            lastInsertedId = lastInsertedId ? lastInsertedId : 0;
+            userObject.id = ++lastInsertedId;
+        }
+
+        localStorage.setItem("lastInsertedId", lastInsertedId);
+        localStorage.setItem(userObject.email, JSON.stringify(userObject));
+    }
+    static getAllObjects = function () {
+        var archive = {}, // Notice change here
+            keys = Object.keys(localStorage),
+            i = keys.length;
+
+        while (i--) {
+            archive[keys[i]] = localStorage.getItem(keys[i]);
+        }
+        return archive;
+    }
+    static getObjectbyEmail = function (email) {
+
+        var archive = localStorage.getItem(email);
+
+        return archive;
+    }
 }
